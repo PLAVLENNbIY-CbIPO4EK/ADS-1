256fbfb162bde964be3c2f92f8a436631375562d
PLAVLENNbIY-CbIPO4EK <ilya.potor2713@gmail.com>
2026-02-28 21:13:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,28 +14,31 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-  int k = 0;
-  if (n <= 1)
-    return -1;
-  for (int i = 2; i < n, ++i) {
-    if (n % i == 0)
-      return -1;
-    else 
-      k += 1;
-  } 
-  return printf(k, "-е простое число");
+    int count = 0, number = 2;
+    while (count < n) {
+        int prime = 1;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                prime = 0;
+                break;
+            }
+        }
+        if (prime) count++;
+        number++;
+    }
+    return number - 1;
 }
 
+
 uint64_t nextPrime(uint64_t value) {
-  while (true) {
-    uint64_t value += 1;
-    for (int i = 2; i < value, ++i) {
-      if (value % i == 0)
-        return -1;
+    int d = 0;
+    while (true) {
+        if (value - d >= 2 && prime(value - d))
+            return value - d;
+        if (prime(value + d))
+            return value + d;
+        d++;
     }
-    return value;
-    break;
-  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
