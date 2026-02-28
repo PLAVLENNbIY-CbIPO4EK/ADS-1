085205e690bedc757e98a4301e8275aad85c7cb5
PLAVLENNbIY-CbIPO4EK <ilya.potor2713@gmail.com>
2026-02-28 21:48:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,62 +6,74 @@
 bool checkPrime(uint64_t value) {
   if (value <= 1)
     return false;
-  for (int i = 2; i < value, ++i) {
+
+  if (value == 2)
+    return true;
+
+  if (value % 2 == 0)
+    return false;
+
+  for (uint64_t i = 3; i <= value / i; i += 2) {
     if (value % i == 0)
       return false;
-  } 
+  }
+
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-    int count = 0, number = 2;
-    while (count < n) {
-        int prime = 1;
-        for (int i = 2; i < number; i++) {
-            if (number % i == 0) {
-                prime = 0;
-                break;
-            }
-        }
-        if (prime) count++;
-        number++;
-    }
-    return number - 1;
+  if (n == 0)
+    return 0;
+
+  uint64_t count = 0;
+  uint64_t current = 1;
+
+  while (count < n) {
+    current++;
+
+    if (checkPrime(current))
+      count++;
+  }
+
+  return current;
 }
 
-
 uint64_t nextPrime(uint64_t value) {
-    int d = 0;
-    while (true) {
-        if (value - d >= 2 && prime(value - d))
-            return value - d;
-        if (prime(value + d))
-            return value + d;
-        d++;
-    }
+  uint64_t current = value + 1;
+
+  while (true) {
+    if (checkPrime(current))
+      return current;
+
+    current++;
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-   int sum = 0;
-   for (int i = 2; i < hbound; ++i) {
-       bool p = true;
-       for (int j = 2; j * j <= i; ++j) {
-           if (i % j == 0) {
-               p = false;
-               break;
-           }
-       }
-       if (p) sum += i;
-   }
-   return sum;
+  uint64_t sum = 0;
+
+  for (uint64_t i = 2; i < hbound; ++i) {
+    if (checkPrime(i))
+      sum += i;
+  }
+
+  return sum;
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-   int k = 0;
-   for (int i = lbound; i <= hbound - 2; ++i) {
-       if (checkPrime(i) && checkPrime(i + 2)) {
-           k++;
-       }
-   }
-   return k;
+  if (hbound < 3)
+    return 0;
+
+  uint64_t k = 0;
+  uint64_t start = lbound;
+
+  if (start < 3)
+    start = 3;
+
+  for (uint64_t i = start; i + 2 < hbound; ++i) {
+    if (checkPrime(i) && checkPrime(i + 2))
+      k++;
+  }
+
+  return k;
 }
